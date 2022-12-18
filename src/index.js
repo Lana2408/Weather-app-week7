0e794bb32c349e6ee5d4992fe9bcbd6206c49a2a
Lana2408 <lana.grigorik@gmail.com>
2022-12-18 19:49:48 +0000

    Added relevant icon from the API

--- src/index.js
+++ src/index.js
@@ -21,16 +21,22 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
 temperatureElement.innerHTML = Math.round(responce.data.main.temp);
 cityElement.innerHTML = responce.data.name;
 descriptionElement.innerHTML = responce.data.weather[0].description;
 humidityElement.innerHTML = responce.data.main.humidity;
 windElement.innerHTML = Math.round(responce.data.wind.speed);
 dateElement.innerHTML = formateDate(responce.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", responce.data.weather[0].description);
+
 }
 
   let apiKey = "d4cf6a64528c35bcb21c2761bd709af2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+  let city = "Kyiv";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayTemperature);
 
