3c61dc227e18d6b07d2f6560a129018991437680
Lana2408 <lana.grigorik@gmail.com>
2022-12-19 01:13:27 +0000

    Implemented the unit conversion

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function formateDate(timestamp) {
 let date = new Date(timestamp);
 let hours = date.getHours();
 if (hours < 10) {
-    hours = `0${minutes}`;
+    hours = `0${hours}`;
 }
 let minutes = date.getMinutes();
 if (minutes < 10) {
@@ -22,6 +22,10 @@ let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
+
+celsiusTemperature = responce.data.main.temp;
+
+
 
 temperatureElement.innerHTML = Math.round(responce.data.main.temp);
 cityElement.innerHTML = responce.data.name;
@@ -47,10 +51,35 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
-search("Kyiv");
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Kyiv");
 
 
